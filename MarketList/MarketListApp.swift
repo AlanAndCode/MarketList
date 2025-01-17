6ede1c2d7db2dc0c1859e40a288f85935c7350c6
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2025-01-17 14:31:46 -0300

    Initial Commit

---
+++ MarketList/MarketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketListApp.swift
+//  MarketList
+//
+//  Created by Alan Anderson Da silva Modesto on 17/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct MarketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarketListTests/MarketListTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarketListTests.swift
+//  MarketListTests
+//
+//  Created by Alan Anderson Da silva Modesto on 17/01/2025.
+//
+
+import Testing
+@testable import MarketList
+
+struct MarketListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
