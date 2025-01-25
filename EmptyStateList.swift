339594d22a55d5dde9c451d9a26073d5050e865a
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2025-01-25 01:47:07 -0300

    feat: creating EmptyStateView to first use

---
+++ EmptyStateList.swift
@@ -0,0 +1,21 @@
+//
+//  Lista.swift
+//  MarketList
+//
+//  Created by Alan Anderson Da silva Modesto on 17/01/2025.
+//
+
+import SwiftUI
+
+struct Lista: View {
+    var body: some View {
+        Text("")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.green)
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    Lista()
+}
