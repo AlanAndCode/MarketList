c7f27c4f16a4abd4bfa451334fbb01661592e1df
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2025-01-30 02:04:28 -0300

    feat: add func moverItemParaEstoque to move itens when have 1 or more unit to estoque

--- MarketList/ViewModels/ListaDeComprasViewModel.swift
+++ MarketList/ViewModels/ListaDeComprasViewModel.swift
@@ -4,32 +4,35 @@
 //
 //  Created by Alan Anderson Da silva Modesto on 24/01/2025.
 //
-
 import Foundation
 import SwiftUI
 
 class ListaDeComprasViewModel: ObservableObject {
-    @Published var itensEmFalta: [Item] = []
     @Published var itensEmEstoque: [Item] = []
     @Published var itensEmLista: [Item] = []
+    
+    private var estoqueViewModel: CardEstoqueViewModel
 
-    // Função para adicionar um item à lista em falta
-    func adicionarItemEmFalta(nome: String, quantidade: Int) {
-        let novoItem = Item(nome: nome, quantidade: quantidade, status: .emFalta)
-        itensEmFalta.append(novoItem)
+    init(estoqueViewModel: CardEstoqueViewModel) {
+        self.estoqueViewModel = estoqueViewModel
     }
 
-    // Função para adicionar um item à lista em estoque
-    func adicionarItemEmEstoque(nome: String, quantidade: Int) {
-        let novoItem = Item(nome: nome, quantidade: quantidade, status: .emEstoque)
-        itensEmEstoque.append(novoItem)
+    func incrementarQuantidade(item: Item) {
+        if let index = itensEmLista.firstIndex(where: { $0.id == item.id }) {
+            itensEmLista[index].quantidade += 1
+            
+            moverItemParaEstoque(item: itensEmLista[index])
+            itensEmLista.remove(at: index)
+        }
     }
-
-    // Função para adicionar um item à lista de compras
+    
+    private func moverItemParaEstoque(item: Item) {
+        estoqueViewModel.adicionarItem(item: item)
+        print("📦 Item movido para o estoque: \(item.nome)")
+    }
+    
     func adicionarItemEmLista(nome: String, quantidade: Int) {
         let novoItem = Item(nome: nome, quantidade: quantidade, status: .emLista)
         itensEmLista.append(novoItem)
     }
 }
-
-
