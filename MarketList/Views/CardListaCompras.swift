99757451732b7650806a020f7ecc331dadb86cca
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2025-01-30 19:04:42 -0300

    feat: add minus button in cardListaCompras

--- MarketList/Views/CardListaCompras.swift
+++ MarketList/Views/CardListaCompras.swift
@@ -10,7 +10,6 @@ import SwiftUI
 
 struct CardListaCompras: View {
     var nome: String
-    @State var quantidade = 0
     @ObservedObject var viewModel: ListaDeComprasViewModel
 
     var body: some View {
@@ -18,47 +17,64 @@ struct CardListaCompras: View {
             Color.green
                 .ignoresSafeArea()
 
-            VStack {
-                HStack {
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text(nome)
-                            .font(.headline)
+            ScrollView {
+                VStack(spacing: 12) {
+                    if viewModel.itensEmLista.isEmpty {
+                        Text("Nenhum item na lista de compras")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding()
+                    } else {
+                        ForEach(viewModel.itensEmLista) { item in
+                            HStack {
+                                VStack(alignment: .leading, spacing: 8) {
+                                    Text(item.nome)
+                                        .font(.headline)
+                                        .frame(maxWidth: .infinity, alignment: .leading)
 
-                        Text("Quantidade: \(quantidade)")
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
-                    }
+                                    Text("Quantidade: \(item.quantidade)")
+                                        .font(.subheadline)
+                                        .foregroundColor(.secondary)
+                                }
 
-                    Spacer()
+                                Spacer()
 
-                    Button(action: {
-                        quantidade += 1
-                    }) {
-                        Image(systemName: "plus.circle")
-                            .font(.title2)
-                            .foregroundColor(.green)
-                    }
-
-                    Button(action: {
-                        if quantidade > 0 { 
-                            quantidade -= 1
+                                Button(action: {
+                                    viewModel.incrementarQuantidade(item: item)
+                                }) {
+                                    Image(systemName: "plus.circle")
+                                        .font(.title2)
+                                        .foregroundColor(.green)
+                                }
+                                
+                                Button(action: {
+                                    viewModel.decrementarQuantidade(item: item)
+                                }) {
+                                    Image(systemName: "minus.circle")
+                                        .font(.title2)
+                                        .foregroundColor(.red)
+                                }
+                            }
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
+                            .padding(.horizontal)
                         }
-                    }) {
-                        Image(systemName: "minus.circle")
-                            .font(.title2)
-                            .foregroundColor(.red)
                     }
                 }
-                .padding()
-                .background(Color.white)
-                .cornerRadius(10)
-                .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
-                .padding(.horizontal)
             }
         }
     }
 }
 
+
 #Preview {
-    CardListaCompras(nome: "Arroz", viewModel: ListaDeComprasViewModel())
+    let estoqueViewModel = CardEstoqueViewModel()
+    let listaDeComprasViewModel = ListaDeComprasViewModel(estoqueViewModel: estoqueViewModel)
+    
+    listaDeComprasViewModel.adicionarItemEmLista(nome: "Arroz", quantidade: 0)
+    listaDeComprasViewModel.adicionarItemEmLista(nome: "Feijão", quantidade: 0)
+    
+    return CardListaCompras(nome: "Vamos Comprar", viewModel: listaDeComprasViewModel)
 }
