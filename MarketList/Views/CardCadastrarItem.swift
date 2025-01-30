820d6e17d2784b95e9dcaa00131da1897615f63a
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2025-01-30 00:45:50 -0300

    feat: Changing Hstack to Vstack and passa viewmodel. name of item to use viewmodel itens

--- MarketList/Views/CardCadastrarItem.swift
+++ MarketList/Views/CardCadastrarItem.swift
@@ -9,72 +9,49 @@
 import SwiftUI
 
 struct CardCadastrarItem: View {
-    @State var nomeProduto: String = ""
-    @State var quantidade: String = ""
     @ObservedObject var viewModel: CardCadastrarItemViewModel
+    
     var body: some View {
         ZStack {
             Color.yellow
                 .ignoresSafeArea()
-
+            
             VStack {
+                Text("Cadastrar Item")
+                    .font(.headline)
+                    .padding(.bottom, 8)
                 
-                HStack {
-                    Image(systemName: "cart")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100, height: 100)
-                        .foregroundColor(.black)
-                        .padding(.leading, 20)
-
-                    Spacer()
-                }
-                .padding(.bottom, 40)
-
-                VStack(alignment: .leading, spacing: 16) {
-                    Text("Cadastrar Item")
-                        .font(.headline)
-                        .padding(.bottom, 8)
-
-                    TextField("Nome do Produto", text: $nomeProduto)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.horizontal)
-
-                    TextField("Quantidade", text: $quantidade)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.horizontal)
-                        .keyboardType(.numberPad)
-
-                    Button(action: {
-                        cadastrarItem()
-                    }) {
-                        HStack {
-                            Spacer()
-                            Text("Cadastrar")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                                .padding()
-                            Spacer()
-                        }
-                        .background(Color.blue)
-                        .cornerRadius(8)
-                    }
+                TextField("Nome do Produto", text: $viewModel.nomeProduto)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding(.horizontal)
+                
+                TextField("Quantidade", text: $viewModel.quantidade)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                    .keyboardType(.numberPad)
+
+                Button(action: {
+                    viewModel.cadastrarItem()
+                }) {
+                    HStack {
+                        Spacer()
+                        Text("Cadastrar")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                        Spacer()
+                    }
+                    .background(Color.blue)
+                    .cornerRadius(8)
                 }
-                .padding()
-                .background(Color.white)
-                .cornerRadius(12)
-                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
                 .padding(.horizontal)
             }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(12)
+            .shadow(radius: 5)
+            .padding(.horizontal)
         }
     }
-
-    private func cadastrarItem() {
-        print("Produto: \(nomeProduto), Quantidade: \(quantidade)")
-    }
 }
 
-#Preview {
-    CardCadastrarItem(viewModel: CardCadastrarItemViewModel())
-}
