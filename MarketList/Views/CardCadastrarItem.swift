264d306f142dbb3523ca4d0b5c8269a3fd904b77
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2025-01-31 02:05:41 -0300

    fix: returning to the old screen of CardCadastrar

--- MarketList/Views/CardCadastrarItem.swift
+++ MarketList/Views/CardCadastrarItem.swift
@@ -17,41 +17,58 @@ struct CardCadastrarItem: View {
                 .ignoresSafeArea()
             
             VStack {
-                Text("Cadastrar Item")
-                    .font(.headline)
-                    .padding(.bottom, 8)
-                
-                TextField("Nome do Produto", text: $viewModel.nomeProduto)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.horizontal)
-                
-                TextField("Quantidade", text: $viewModel.quantidade)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.horizontal)
-                    .keyboardType(.numberPad)
-
-                Button(action: {
-                    viewModel.cadastrarItem()
-                }) {
-                    HStack {
-                        Spacer()
-                        Text("Cadastrar")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .padding()
-                        Spacer()
-                    }
-                    .background(Color.blue)
-                    .cornerRadius(8)
+                HStack {
+                    Image(systemName: "cart")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.black)
+                        .padding(.leading, 20)
+                    
+                    Spacer()
                 }
+                .padding(.bottom, 40)
+                
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("Cadastrar Item")
+                        .font(.headline)
+                        .padding(.bottom, 8)
+                    
+                    TextField("Nome do Produto", text: $viewModel.nomeProduto)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+                    
+                    TextField("Quantidade", text: $viewModel.quantidade)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+                        .keyboardType(.numberPad)
+                    
+                    Button(action: {
+                        viewModel.cadastrarItem()
+                    }) {
+                        HStack {
+                            Spacer()
+                            Text("Cadastrar")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                            Spacer()
+                        }
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                    }
+                    .padding(.horizontal)
+                }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(12)
+                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
                 .padding(.horizontal)
             }
-            .padding()
-            .background(Color.white)
-            .cornerRadius(12)
-            .shadow(radius: 5)
-            .padding(.horizontal)
         }
     }
 }
 
+#Preview {
+    CardCadastrarItem(viewModel: CardCadastrarItemViewModel(estoqueViewModel: CardEstoqueViewModel()))
+}
